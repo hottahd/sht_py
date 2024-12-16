0c1b29635e397e4f739778f8816ecf0f546950cc
Hideyuki Hotta <hotta.hideyuki@gmail.com>
2024-12-16 22:56:34 +0900

    README.mdに例を追加し、テストスクリプトのサンプリングサイズを増加。テストデータ生成方法を修正。

--- README.md
+++ README.md
@@ -1,16 +1,11 @@
 # sht_py
 Spherical Harmonic Expansion routine in python+fortran
 
-## Files
-- sht.sht: functions using ctype (faster and parallerized)
-- sht.sht_py: pure python functions
-
 ## Install
 ### Requirement
 You need `make` and`gfortran`.
 
 Debian-based (Debian, Ubuntu, Mint, etc…)
-
 ```shell
 sudo apt update
 sudo apt-get install -y build-essential gfortran
@@ -36,5 +31,26 @@ export OMP_NUM_THREADS=4
 
 For MacOS X, it does not work and we cannot use large size of quantities for the transformation.
 
-## functions
+## Functions
 please see ```help(sht.sht_py)``` and ```help(sht.sht)```
+
+## Example
+
+```python
+import numpy as np
+import sht
+
+th = np.linspace(0,  np.pi, 128)
+ph = np.linspace(0,2*np.pi, 256)
+
+TH, PH = np.meshgrid(th, ph, indexing='ij')
+
+qq = np.sin(TH)*np.cos(PH)
+
+# forward expansion
+fqq = sht.sht(qq, th, ph)
+# backward expansion
+qqb = sht.sht(fqq, th, ph, direction = -1)
+
+
+```
--- tests/test.py
+++ tests/test.py
@@ -2,21 +2,37 @@ import numpy as np
 import sht
 
 
-ith = 128
-jph = 256
+ith = 512
+jph = 1024
 
-th = np.linspace(0,  np.pi, ith)
-ph = np.linspace(0,2*np.pi, jph)
+dth = np.pi/ith
+dph = 2*np.pi/jph
+
+th = np.zeros(ith)
+th[0] = 0.5*dth
+for i in range(1, ith):
+    th[i] = th[i-1] + dth
+
+ph = np.zeros(jph)
+ph[0] = 0.5*dph
+for j in range(1, jph):
+    ph[j] = ph[j-1] + dph
 
 TH, PH = np.meshgrid(th, ph, indexing='ij')
 
-qq = np.zeros((ith, jph))
-for i in range(ith//2):
-    for j in range(jph//2):
-        amp = np.random.uniform(-1,1)/ith/jph
-        qq += amp*np.sin(i*TH)*np.sin(j*PH)
+qq = np.sin(10*TH)*np.cos(5*PH) + np.cos(5*TH)*np.sin(3*PH)
 
+# qq = np.zeros((ith, jph))
+# for i in range(ith//2):
+#     print(i)
+#     for j in range(jph//2):
+#         amp1 = np.random.uniform(-1,1)/ith/jph
+#         amp2 = np.random.uniform(-1,1)/ith/jph
+#         amp3 = np.random.uniform(-1,1)/ith/jph
+#         amp4 = np.random.uniform(-1,1)/ith/jph
+#         qq += (amp1*np.sin(i*TH) + amp2*np.cos(i*TH)) \
+#             *(amp3*np.sin(j*PH) + amp4*np.cos(j*PH))
 fqq = sht.sht_py(qq, th, ph)
 qqb = sht.sht_py(fqq, th, ph,direction=-1)
 
-print((qq - qqb).mean())
+print(((qq - qqb)).mean())
